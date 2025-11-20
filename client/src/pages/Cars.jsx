1cbf5470bc3955bf1070ff46d0b051be42bdd3ad
developer-abdiweli-jama <abdiwelijamac@gmail.com>
2025-11-21 00:32:35 +0300

    feat: enhance application layout with new components and routing, including Footer, Banner, Testimonial, and Newslatter

--- client/src/pages/Cars.jsx
+++ client/src/pages/Cars.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import CarDetails from './CarDetails'
 
 const Cars = () => {
   return (
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import FeaturedSection from '../components/FeaturedSection'
+import Banner from '../components/Banner'
+import Testimonial from '../components/Testimonial'
+import Newslatter from '../components/Newslatter'
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
     <>
       <Hero/>
+      <FeaturedSection/>
+      <Banner/>
+      <Testimonial/>
+      <Newslatter/>
     </>
   )
 }
